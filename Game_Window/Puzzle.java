7deb0dd5605f306d2c3c9be00375a6ab1ac96a6d
Md Mynuddin <60794460+Mynuddin-dev@users.noreply.github.com>
2020-03-10 12:04:31 +0600

    Delete Unnecessary Comment

--- Game_Window/Puzzle.java
+++ Game_Window/Puzzle.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Game_Window;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
